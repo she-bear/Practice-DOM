82e9436a77ada888edfcd8c9d4ad340cc8fdc725
Olga Kutueva <kutueva.olga@gmail.com>
2023-11-25 19:08:45 +0300

    Workshop 3, задание 1, заготовка

---
+++ WORKSHOP_3/task_1.js
@@ -0,0 +1,65 @@
+const productsData = [
+    {
+        id: 1,
+        name: "Ноутбук",
+        category: "Электроника",
+    },
+    {
+        id: 2,
+        name: "Смартфон",
+        category: "Электроника",
+    },
+    {
+        id: 3,
+        name: "Кофемашина",
+        category: "Бытовая техника",
+    },
+    {
+        id: 4,
+        name: "Фотокамера",
+        category: "Электроника",
+    },
+    {
+        id: 5,
+        name: "Микроволновая печь",
+        category: "Бытовая техника",
+    },
+    {
+        id: 6,
+        name: "Книга",
+        category: "Книги",
+    },
+    {
+        id: 7,
+        name: "Футболка",
+        category: "Одежда",
+    },
+    {
+        id: 8,
+        name: "Шапка",
+        category: "Одежда",
+    },
+    {
+        id: 9,
+        name: "Стул",
+        category: "Мебель",
+    },
+    {
+        id: 10,
+        name: "Стол",
+        category: "Мебель",
+    },
+];
+
+const selectElement = document.querySelector('#category-select');
+
+selectElement.onchange = function () {
+    const selectedCategory = selectElement.value;
+    console.log(selectedCategory);
+
+    let
+
+    // switch (selectedCategory) {
+
+    // }
+}
